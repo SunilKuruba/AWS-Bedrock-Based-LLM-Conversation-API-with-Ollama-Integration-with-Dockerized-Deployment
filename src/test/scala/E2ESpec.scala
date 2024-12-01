4c08d1ad23a4d295f0b60338cb89d61c470c0c8c
sunilkuruba <skuru@uic.edu>
2024-12-01 05:03:07 -0600

    update README file

--- src/test/scala/E2ESpec.scala
+++
@@ -1,9 +0,0 @@
-import org.scalatest.flatspec.AnyFlatSpec
-import org.scalatest.matchers.should.Matchers
-
-class E2ESpec extends AnyFlatSpec with Matchers{
-  "End-to-end program" should "work with param args" in {
-    val seedToken = "Hey there!"
-    OllamaAPIClient.main(Array(seedToken))
-  }
-}
